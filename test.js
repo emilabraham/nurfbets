aae9466462ddd60a649d98e3c4db75a01adba072
Emil Abraham <emilabraham1414@gmail.com>
2015-04-01 10:22:08 -0400

    Updated test to show the returned json blob and duration of match

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 var request = require('request');
 
 var options = {
-  url: 'https://na.api.pvp.net/observer-mode/rest/featured',
+  url: 'https://na.api.pvp.net/api/lol/na/v2.2/match/1778704162',
   qs: {
+    includeTime: true,
     api_key: 'fce87c83-d6f4-4e5f-98ab-da74bb6448ba'
   }
 }
@@ -14,6 +15,8 @@ function callback (error, response, body) {
 
   var parsed = JSON.parse(body);
   console.log(parsed);
+  var elapsedTime = Math.floor(parsed.matchDuration/60);
+  console.log('The game lasted for ' + elapsedTime + ' minutes');
 }
 
 request.get(options, callback);
